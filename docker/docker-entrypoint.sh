b0bfafb3b1a4eabd555616da3056af43338d5a08
gabrieljuliao <gabrielbjuliao@gmail.com>
2025-02-01 12:58:23 -0300

    Add support to context path

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -26,8 +26,37 @@ else
     CONFIG_FILE="$NGINX_CONFIG_PLAINTEXT_PATH"
 fi
 
-# Start Nginx with the appropriate configuration file dynamically
-log_logstash "INFO" "Starting Nginx with $CONFIG_FILE..."
+# Validate that CONTEXT_PATH is set
+if [ -z "$CONTEXT_PATH" ]; then
+    log_logstash "ERROR" "CONTEXT_PATH is not set. Exiting..."
+    exit 1
+fi
+
+log_logstash "INFO" "Using CONTEXT_PATH: $CONTEXT_PATH"
+log_logstash "INFO" "Preparing Nginx configuration..."
+
+# Use a predefined directory inside the container
+CONFIG_DIR="/tmp/nginx"
+mkdir -p "$CONFIG_DIR"
+
+# Generate a unique filename for the config
+NEW_CONFIG_FILE="$CONFIG_DIR/nginx_config_$(date +%s).conf"
+cp "$CONFIG_FILE" "$NEW_CONFIG_FILE"
+
+log_logstash "INFO" "Nginx configuration file created: $NEW_CONFIG_FILE"
+
+# Ensure `envsubst` works by replacing variables in the new config file
+log_logstash "INFO" "Replacing variables in Nginx configuration..."
+envsubst '${CONTEXT_PATH}' < "$CONFIG_FILE" > "$NEW_CONFIG_FILE"
+
+# Validate if the new config file is not empty after substitution
+if [ ! -s "$NEW_CONFIG_FILE" ]; then
+    log_logstash "ERROR" "Nginx configuration became empty after substitution! Exiting..."
+    rm -f "$NEW_CONFIG_FILE"
+    exit 1
+fi
+
+log_logstash "INFO" "Starting Nginx with config: $NEW_CONFIG_FILE"
 
 # Redirect all further output, validating if each line is JSON before processing
 exec > >(while read -r line; do
@@ -44,4 +73,5 @@ done) 2> >(while read -r line; do
     fi
 done)
 
-exec /docker-entrypoint.sh nginx -c "$CONFIG_FILE" -g "daemon off;"
+# Start Nginx using the new config file
+exec /docker-entrypoint.sh nginx -c "$NEW_CONFIG_FILE" -g "daemon off;"
